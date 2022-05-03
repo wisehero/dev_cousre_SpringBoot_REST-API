49234005c793bb809b21f4154f220f2f83e3f94f
wisehero <godjw96@gmail.com>
2022-05-03 15:46:59 +0900

    # 프로젝트 초기 셋팅

---
+++ src/main/java/RestWebService/springrestweb/SpringrestwebApplication.java
@@ -0,0 +1,13 @@
+package RestWebService.springrestweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringrestwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringrestwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/RestWebService/springrestweb/SpringrestwebApplicationTests.java
@@ -0,0 +1,13 @@
+package RestWebService.springrestweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringrestwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
