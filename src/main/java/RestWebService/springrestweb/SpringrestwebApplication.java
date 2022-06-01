4a0eca5765f0b0986810e57f9994d783da874efa
wisehero10 <godjw96@gmail.com>
2022-06-01 19:29:38 +0900

    # 설정 정보 수정

--- src/main/java/RestWebService/springrestweb/SpringrestwebApplication.java
+++ src/main/java/RestWebService/springrestweb/SpringrestwebApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringrestwebApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringrestwebApplication.class, args);
 	}
-
 }
